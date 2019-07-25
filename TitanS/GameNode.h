89a0e4e8647649b9f848e85a36060dffa9d98813
huie.kim <huie.kim@kakaocorp.com>
2019-07-25 17:08:20 +0900

    no message

--- TitanS/GameNode.h
+++ TitanS/GameNode.h
@@ -8,7 +8,7 @@ class GameNode
 private:
 	HDC _hdc;				// 화면 HDC
 	bool _managerInit;		// 매니저 초기화 여부
-
+   
 public:
 	virtual HRESULT init();	// void를 사용해도 됨. HRESULT: 정상적인 종료 여부 등의 여러 값을 가질 수 있음. 제일 많이 사용하는 것이 S_OK(정상 종료), E_FAIL(에러 발생)
 	virtual HRESULT init(bool managerInit);
